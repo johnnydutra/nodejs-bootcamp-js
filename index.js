76db7d80051c4bdb68ac1dfe809deceb0473eddf
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-20 17:21:55 -0300

    lecture 10: reading and writing files asynchronously

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textInput);
+// Blocking, sync
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textInput);
+// const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOutput);
+// console.log('File written!');
 
-const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOutput);
-console.log('File written!');
+// Non-blocking, async
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR!');
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    if (err) return console.log('ERROR!');
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      if (err) return console.log('ERROR!');  
+      console.log(data3);
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}` ,(err) => {
+        if (err) return console.log('ERROR!');
+        console.log('Your file has been written');
+      });
+    });
+  });
+});
