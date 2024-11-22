c95092d385277b22092fe12e439d43aa407506f6
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-22 11:30:15 -0300

    60: using 3rd party middleware

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -95,6 +102,8 @@ const deleteTour = (req, res) => {
   });
 }
 
+// 3) ROUTES
+
 app.route('/api/v1/tours')
   .get(getAllTours)
   .post(createTour);
@@ -103,6 +112,8 @@ app.route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+  // 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
