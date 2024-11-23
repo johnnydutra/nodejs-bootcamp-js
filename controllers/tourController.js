827cec484e43a352eeee8ac4a9390f9d3916ff97
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-23 08:33:48 -0300

    91: updating documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,6 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
-
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -22,7 +21,6 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-
     res.status(200).json({
       status: 'success',
       data: {
@@ -40,7 +38,6 @@ exports.getTour = async (req, res) => {
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
-
     res.status(201).json({
       status: 'success',
       data: {
@@ -55,13 +52,23 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
