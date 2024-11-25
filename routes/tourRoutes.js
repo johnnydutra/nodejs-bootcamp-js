35626ad55aa8a0b02332b6c3dec83e8729bf4daf
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-25 12:03:21 -0300

    102: aggregation pipeline matching and grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,15 @@ const {
   deleteTour,
   getAllTours,
   getTour,
+  getTourStats,
   updateTour,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 
