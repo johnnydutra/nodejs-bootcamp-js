d3736c9c9e8fba55369275920394cd3cb694ea3a
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-28 14:19:11 -0300

    142: sending jwt via cookie

--- utils/token.js
+++ utils/token.js
@@ -13,6 +13,18 @@ const signToken = (id) => {
 
 const sendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + parseInt(process.env.JWT_COOKIE_EXP)),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
