56a811d185e8e2cc9c29874188d996d7337cbe4c
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-02 08:03:31 -0300

    171: geospatial queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,7 @@ const factory = require('./handlerFactory');
 const Tour = require('../models/tourModel');
 
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -89,6 +90,33 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     status: 'success',
     data: {
       plan,
+    },
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  if (!lat || !lng) {
+    next(new AppError('Please provide latitude and longitude in the lat,lng format', 400));
+  }
+
+  const radius = unit === 'mi' ? distance / 3962.2 : distance / 6378.1;
+
+  const startLocation = {
+    $geoWithin: {
+      $centerSphere: [[lng, lat], radius],
+    },
+  };
+
+  const tours = await Tour.find({ startLocation });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
     },
   });
 });
