bcfc631db60c6f832a9e6cfe81fb72ef72a0587c
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-02 09:45:14 -0300

    181: setting up project structure

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const reviewRoutes = require('./routes/reviewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -69,28 +70,11 @@ app.use((req, res, next) => {
 });
 
 // Routers
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRoutes);
+app.use('/', viewRouter);
 
 // Resource not found fallback
 app.all('*', (req, res, next) => {
